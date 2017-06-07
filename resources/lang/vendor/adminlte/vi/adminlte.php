0ccea078bf4f8c3330cfbcc2cc36c2363de2e58a
haiph95 <zdt4gjpnfmxes>
2017-06-07 10:11:42 +0700

    Init Project

---
+++ resources/lang/vendor/adminlte/vi/adminlte.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'full_name'                   => 'Họ Tên',
+    'email'                       => 'Email',
+    'password'                    => 'Mật khẩu',
+    'retype_password'             => 'Xác nhận mật khẩu',
+    'remember_me'                 => 'Lưu đăng nhập',
+    'register'                    => 'Đăng ký',
+    'register_a_new_membership'   => 'Đăng ký thành viên mới',
+    'i_forgot_my_password'        => 'Quên mật khẩu',
+    'i_already_have_a_membership' => 'Đã có tài khoản? Đăng nhập ngay',
+    'sign_in'                     => 'Đăng nhập',
+    'log_out'                     => 'Đăng xuất',
+    'toggle_navigation'           => 'Toggle navigation',
+    'login_message'               => 'Đăng nhập để bắt đầu sử dụng',
+    'register_message'            => 'Đăng ký thành viên mới',
+    'password_reset_message'      => 'Reset Password',
+    'reset_password'              => 'Reset Password',
+    'send_password_reset_link'    => 'Send Password Reset Link',
+    'sign_in_facebook'            => 'Đăng nhập bằng Facebook',
+    'sign_in_google'              => 'Đăng nhập bằng Google',
+];
